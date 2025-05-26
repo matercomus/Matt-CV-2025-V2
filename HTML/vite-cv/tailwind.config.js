2c7fe6ffae6575851025367f41cf3e96869e2da0
matercomus <matek.kedzia@gmail.com>
2025-05-26 18:18:12 +0800

    using vite for the web cv

---
+++ HTML/vite-cv/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "../Mateusz_Kedzia_AI_Engineer_CV_zh.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
